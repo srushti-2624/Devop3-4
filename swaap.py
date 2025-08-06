17bd2af3e59cc9d83b4293cffcaa042c0f1857b7
srushti-2624 <srushtikpnaik@gmail.com>
2025-08-06 08:34:59 +0530

    Create swaap.py

---
+++ swaap.py
@@ -0,0 +1,12 @@
+# Initial values
+a = 5
+b = 10
+
+print(f"Before swapping: a = {a}, b = {b}")
+
+# Swapping using a temporary variable
+temp = a
+a = b
+b = temp
+
+print(f"After swapping: a = {a}, b = {b}")
